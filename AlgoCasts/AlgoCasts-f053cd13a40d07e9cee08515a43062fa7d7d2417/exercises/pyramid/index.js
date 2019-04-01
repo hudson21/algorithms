b1bf142154791dfac24e88b47a77d78ef4aa1f73
hudson21 <camihudsondiaz@ittepic.edu.mx>
2019-03-31 22:16:20 -0600

    49. What is Runtime Complexity

--- AlgoCasts/AlgoCasts-f053cd13a40d07e9cee08515a43062fa7d7d2417/exercises/pyramid/index.js
+++ AlgoCasts/AlgoCasts-f053cd13a40d07e9cee08515a43062fa7d7d2417/exercises/pyramid/index.js
@@ -14,6 +14,64 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+
+
+/*
+n3
+
+    c1  c2  c3  c4  c5 
+r1  -   -   #   -   -
+
+r2  -   #   #   #   -
+
+r3  #   #   #   #   #
+
+
+*/
+
+function pyramid(n, row = 0, level = '') {
+    if (row === n) {
+        return;
+    }
+
+    if (level.length === 2 * n - 1) {
+        console.log(level);
+        return pyramid(n, row + 1);
+    }
+
+    const midPoint = Math.floor((n * 2 - 1) / 2);
+    let add;
+    if (midPoint - row <= level.length && midPoint + row >= level.length) {
+        add = '#';
+    } else {
+        add = ' ';
+    }
+
+    pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
+
+/*
+Solution 1
+
+function pyramid(n) {
+    //Calculate the mid poit of the row
+    const midPoint = Math.floor((2 * n - 1) / 2);
+
+    for (let row = 0; row < n; row++) {
+        let level = '';
+
+        for (let column = 0; column < 2 * n - 1; column++) {
+            if (midPoint - row <= column && midPoint + row >= column) {
+                level += '#';
+            } else {
+                level += ' ';
+            }        
+        }
+
+        console.log(level);
+    }
+}
+
+*/
--- AlgoCasts/AlgoCasts-f053cd13a40d07e9cee08515a43062fa7d7d2417/exercises/vowels/index.js
+++ AlgoCasts/AlgoCasts-f053cd13a40d07e9cee08515a43062fa7d7d2417/exercises/vowels/index.js
@@ -7,6 +7,82 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+   // g: dont stop in the first match
+   // i: insensitive cases lowerCase and upperCase
+   const matches = str.match(/[aeiou]/gi);
+   return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
+
+/*
+My Solution
+
+function vowels(str) {
+  let splitedStr = str.toLowerCase();
+  let vowelsMap = {};
+  let result = 0;
+
+  for (let vowel of splitedStr) {
+    if(vowel === 'a' || 
+      vowel === 'e' ||
+      vowel === 'i' ||
+      vowel === 'o' ||
+      vowel === 'u') {
+        if(!vowelsMap[vowel]) vowelsMap[vowel] = 1;
+        else {
+          vowelsMap[vowel]++
+        } 
+    } 
+  }
+
+  for (let vowel in vowelsMap) {
+    result += vowelsMap[vowel];
+  }
+
+  return result;
+}
+
+console.log(vowels('Why do you ask?'));
+
+*/
+
+/*
+Another Solution
+
+function vowels(str) {
+  let splitedStr = str.toLowerCase();
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let result = 0;
+
+  for (let vowel of splitedStr) {
+    if (vowels.indexOf(vowel) !== -1) {
+      result++;
+    }
+  }
+
+  return result;
+}
+
+console.log(vowels('Hi There!'));
+
+*/
+
+/*
+Solution 1
+
+function vowels(str) {
+    let count = 0;
+    const checker = ['a', 'e', 'i', 'o', 'u'];
+
+    for (let char of str.toLowerCase()) {
+        if (checker.includes(char)) {
+            count++;
+        } 
+    }
+
+    return count;
+}
+
+*/
