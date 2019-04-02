36c8b5c656c4ec120a46f33112820998d6af4f5c
hudson21 <camihudsondiaz@ittepic.edu.mx>
2019-04-01 23:54:01 -0600

    The Queue Data Structure

--- AlgoCasts/AlgoCasts-f053cd13a40d07e9cee08515a43062fa7d7d2417/exercises/fib/index.js
+++ AlgoCasts/AlgoCasts-f053cd13a40d07e9cee08515a43062fa7d7d2417/exercises/fib/index.js
@@ -8,6 +8,59 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+    const cache = {};
+
+    return function(...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result;
+    };
+}
+
+function slowFib(n) {
+    if (n < 2) return n;
+    
+    return fib(n - 1) + fib(n - 2);
+}
+
+fib = memoize(slowFib);
 
 module.exports = fib;
+
+/*
+Solution 1 (Iterative)  O(n) --> linear Runtime
+
+function fib(n) {
+    const result = [0, 1];  
+
+    for (let i = 2; i <= n; i++) {
+        const a = result[i - 1];//previous element
+        const b = result[i - 2];//previous element - 1
+
+        result.push(a + b);
+    }
+
+    return result[n];
+}
+
+*/
+
+/*
+For each aditional element that we add into our collection or for each increment of the value n to our function we 
+experiencing a dramatic increase in a number of function calls that are required
+
+Solution 2 (with Recursion) This one uses exponential runtime O(2^n)
+
+function fib(n) {
+    if (n < 2) return n;
+    
+    return fib(n - 1) + fib(n - 2);
+}
+
+*/
