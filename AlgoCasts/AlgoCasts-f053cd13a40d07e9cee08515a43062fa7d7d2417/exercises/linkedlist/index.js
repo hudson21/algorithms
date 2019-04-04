9050711a11ed414fc89325119903d3c43fbd9c0a
hudson21 <camihudsondiaz@ittepic.edu.mx>
2019-04-03 23:01:10 -0600

    83. Bye-Bye Tail with RemoveLast

--- AlgoCasts/AlgoCasts-f053cd13a40d07e9cee08515a43062fa7d7d2417/exercises/linkedlist/index.js
+++ AlgoCasts/AlgoCasts-f053cd13a40d07e9cee08515a43062fa7d7d2417/exercises/linkedlist/index.js
@@ -9,6 +9,52 @@ class Node {
     }
 }
 
-class LinkedList {}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+    }
+
+    size() {
+        let counter = 0;
+        let node = this.head;
+
+        while (node) {
+            counter++;
+            node = node.next;
+        }
+
+        return counter;
+    }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        if (!this.head) return null;
+         
+        let node = this.head;
+        while(node) {
+            if(!node.next) return node;
+            node = node.next;
+        }
+
+        return node;
+    }
+
+    clear() {
+        this.head = null;
+    }
+
+    removeFirst() {
+        if (!this.head) return;
+        this.head = this.head.next;
+    }
+}
+
 
 module.exports = { Node, LinkedList };
