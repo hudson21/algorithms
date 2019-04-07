587d333f1a01c9ca703df4b3e7556fe24de6cf4f
hudson21 <camihudsondiaz@ittepic.edu.mx>
2019-04-06 19:06:22 -0600

    107. Tree Implementation

--- AlgoCasts/AlgoCasts-f053cd13a40d07e9cee08515a43062fa7d7d2417/exercises/midpoint/index.js
+++ AlgoCasts/AlgoCasts-f053cd13a40d07e9cee08515a43062fa7d7d2417/exercises/midpoint/index.js
@@ -13,7 +13,15 @@
 //   midpoint(l); // returns { dat  a: 'b' }
 
 function midpoint(list) {
-        
+    let slow = list.getFirst(); // list.head;
+    let fast = list.getFirst();
+
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow;
 }
 
 module.exports = midpoint;
